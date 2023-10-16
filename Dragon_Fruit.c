03ac649b0e7ce660ada80344b2ee018eeb5041c5
23A91A1234 <147469401+23A91A1234@users.noreply.github.com>
2023-10-16 10:46:46 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d", &x);     if(x>2 && x%2==00)     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Dragon_Fruit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    if(x>2 && x%2==00)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}

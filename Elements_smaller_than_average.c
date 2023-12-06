61a4163774c1198e18eb8fa4afd501b3c9a4d4af
23A91A1234 <147469401+23A91A1234@users.noreply.github.com>
2023-12-06 18:16:19 +0530

    #include<stdio.h> int main() {     int n,i,t,s=0,sum=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum+=arr[i];     }     t=sum/n;     for(i=0;i<n;i++)     {         if(t>=arr[i])         {            s++;         }     }     printf("%d",s);
    }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,t,s=0,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    t=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(t>=arr[i])
+        {
+           s++; 
+        }
+    }
+    printf("%d",s);
+    
+}

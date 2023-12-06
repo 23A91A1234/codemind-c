1c134febbdc9bd6215383669fb15d559d55b0aee
23A91A1234 <147469401+23A91A1234@users.noreply.github.com>
2023-12-06 14:56:36 +0530

    #include<stdio.h> int main() {     int n,i,t,count=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]%2!=0)         {             count++;         }     }     for(i=0;i<n;i++)     {         if(i%2!=0&&arr[i]%2!=0)         {             t++;         }     }     if(t==count)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,t,count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]%2!=0)
+        {
+            count++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0&&arr[i]%2!=0)
+        {
+            t++;
+        }
+    }
+    if(t==count)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}

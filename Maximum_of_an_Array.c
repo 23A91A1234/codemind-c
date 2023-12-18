e617d727bd3b578d98aee3077be27d95a9fd5a27
23A91A1234 <147469401+23A91A1234@users.noreply.github.com>
2023-12-18 10:46:44 +0530

    #include<stdio.h> int main() {     int n,i,d;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     d=arr[0];     for(i=1;i<n;i++)     {         if(arr[i]>d)         {             d=arr[i];         }
        }
        printf("%d",d);
    }

---
+++ Maximum_of_an_Array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,d;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    d=arr[0];
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]>d)
+        {
+            d=arr[i];
+        }
+        
+    }
+    printf("%d",d);
+}

44b799b8013332bda23cd99342fffe0c55888b4e
23A91A1234 <147469401+23A91A1234@users.noreply.github.com>
2024-01-02 19:44:59 +0530

    #include<stdio.h> int main() {     int i,count=0;     char str[100];     scanf("%[^ ]s",str);     for(i=-0;str[i]!=NULL;i++)     {         if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')          count++;     }     printf("%d",count); }

---
+++ Vowels_Count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,count=0;
+    char str[100];
+    scanf("%[^
+]s",str);
+    for(i=-0;str[i]!=NULL;i++)
+    {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+         count++;
+    }
+    printf("%d",count);
+}
